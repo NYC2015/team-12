f469157773260f8c50d50da14c8f8787581ce33c
Kevin Ly <kevinhly01@gmail.com>
2015-10-17 05:02:30 -0400

    added prototype date

--- ChildrensAidSociety/js/form-money-slider.js
+++ ChildrensAidSociety/js/form-money-slider.js
@@ -1,3 +1,7 @@
 $('#range').on("change", function() {
     $('.output').val("$" + this.value +"00" )
     }).trigger("change");
+
+$('#date_range').on("change", function() {
+    $('.date_range').val(this.value +" month(s)" )
+    }).trigger("change");
